9a2f20267a2830c042e9d0850a0b37a503281ef9
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-06-26 18:23:14 +0200

    Select year from 2022 to 13 before

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 const resultado = document.querySelector('#resultado');
 
+const year = document.querySelector('#year');
+const max = new Date().getFullYear();
+const min = max - 13;
+
 //eventos
 document.addEventListener('DOMContentLoaded', () => {
     mostrarAutos();
+
+    //Llena las opciones de años
+    llenarSelect();
 })
 
 //Funciones
 
 function mostrarAutos(){
     autos.forEach(auto => {
-        const { marca, modelo, year, puertas, transmision, precio, color }= auto;
+        const { marca, modelo, year, puertas, transmision, precio, color } = auto;
         const autoHTML = document.createElement('p');
         autoHTML.textContent = `${marca}- ${modelo} -${year} -${puertas}Puertas -  Transmision: ${transmision} - Precio -${precio} - Color: ${color} `;
 
@@ -17,4 +24,15 @@ function mostrarAutos(){
         resultado.appendChild(autoHTML);
     
     });
+}
+
+//Genera los años del select
+
+function llenarSelect() {
+    for(i = max; i > min; i--){
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);
+    }
 }
