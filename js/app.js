85dfbf3a9653b5f2e0a67269e02f25a3377517d0
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-06-28 06:40:49 +0200

    creates an object that gets filed with car details

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+//This is a container for the results
 const resultado = document.querySelector('#resultado');
 
-const year = document.querySelector('#year');
+
 const max = new Date().getFullYear();
 const min = max - 13;
+
+
+// Generates objects when search
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
 
 //eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,6 +32,38 @@ document.addEventListener('DOMContentLoaded', () => {
     //Llena las opciones de años
     llenarSelect();
 })
+
+//Event listener for the search forms
+marca.addEventListener('change', e =>{
+   datosBusqueda.marca = e.target.value;
+
+   console.log(datosBusqueda);
+})
+
+year.addEventListener('change', e =>{
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener('change', e =>{
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', e =>{
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change', e =>{
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', e =>{
+    datosBusqueda.transmision = e.target.value;
+});
+color.addEventListener('change', e =>{
+    datosBusqueda.color = e.target.value;
+});
+
+
 
 //Funciones
 
