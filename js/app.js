79bd3c00c8767174f871189ffc869526c9b9ef2c
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-07-04 02:01:46 +0200

    Filter by color, doors and pops a red message when there is no car matching with the filters

--- js/app.js
+++ js/app.js
@@ -54,17 +54,23 @@ minimo.addEventListener('change', e =>{
 
 maximo.addEventListener('change', e =>{
     datosBusqueda.maximo = e.target.value;
+
+    filtrarAuto();
 });
 
 puertas.addEventListener('change', e =>{
-    datosBusqueda.puertas = e.target.value;
+    datosBusqueda.puertas = parseInt(e.target.value);
+    filtrarAuto();
 });
 
 transmision.addEventListener('change', e =>{
     datosBusqueda.transmision = e.target.value;
+
+    filtrarAuto();
 });
 color.addEventListener('change', e =>{
     datosBusqueda.color = e.target.value;
+    filtrarAuto();
 });
 
 
@@ -107,10 +113,23 @@ function llenarSelect() {
 //Functions filters based on search
 
 function filtrarAuto() {
-    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear ).filter( filtrarMinimo );
-    // console.log(resultado);
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear ).filter( filtrarMinimo ).filter( filtrarMaximo ).filter( filtrarPuertas ).filter( filtrarTransmision ).filter(filtrarColor);
+    
 
-    mostrarAutos(resultado);
+    if (resultado.length) {
+        mostrarAutos(resultado);
+    }else{
+        noResultado();
+    }
+}
+
+function noResultado(auto) {
+
+    limpiarHTML();
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'No hay resultados. Intenta con otros terminos de busqueda';
+    resultado.appendChild(noResultado);
 }
 
 function filtrarMarca(auto) {
@@ -138,4 +157,41 @@ function filtrarMinimo(auto) {
     }
     return auto;
     
+}
+
+function filtrarMaximo(auto) {
+    const { maximo } = datosBusqueda;
+
+    if ( maximo ) {
+        return auto.precio <= maximo;
+    }
+    return auto;
+}
+function filtrarPuertas(auto) {
+     const { puertas } = datosBusqueda;
+
+    if ( puertas ) {
+        return auto.puertas === puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+
+    const { transmision } = datosBusqueda;
+
+    if ( transmision ) {
+        return auto.transmision === transmision;
+    }
+    return auto;
+
+}
+
+function filtrarColor(auto) {
+    const { color } = datosBusqueda;
+
+    if ( color ) {
+        return auto.color === color;
+    }
+    return auto;
 }
